7d435f8ddfa245cdf090969d1d6ea0115a956792
Colin Curtis <ckcurtis@renci.org>
2018-07-18 09:30:36 -0400

    updated bionames/ files

--- bionames/bionames_smartapi.sh
+++ bionames/bionames_smartapi.sh
@@ -13,21 +13,11 @@ if [ "x$GIT_PULL" == "xtrue" ]; then
     cd ..
 fi
 
-#ontologies=$(find $DATA_DIR -name "*.obo" -print | wc -l)
-#if [ "$ontologies" == "0" ]; then
-
-    # Reject empty ontology store.
-    #echo Configure the data directory via the DATA_DIR environment setting to be a directory containing obo ontologies.
-    #exit 1
-    
-#else
-
-    # Execute the smartAPI application.
-  #  echo Starting app with ontologies:
-  #  python $PWD/reasoner-tools/greent/api/onto.py \
- #          --port=$APP_PORT \
-  #         --data=$DATA_DIR
-#fi
+#Execute the smartAPI application.
+echo Starting app: bionames
+python $PWD/reasoner-tools/builder/api/naming.py \
+       --port=$APP_PORT \
+       --data=$DATA_DIR
 
 echo Exiting.
 exit 0
